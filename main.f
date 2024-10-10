5260817161bad080ed8eb090adb66df709e85e78
cuamll <callum.gray.10@ucl.ac.uk>
2024-10-10 11:17:24 +0100

    update README, maxdim as parameter, tidy up

--- main.f
+++ main.f
@@ -2,22 +2,23 @@ program main
   use iso_c_binding
   use nnls
   implicit none
-  integer(kind=CI) :: i, j, k, n, m, mode, maxiter, nmax
+  integer(kind=CI) :: i, j, k, n, m, maxdim, mode, maxiter, failures, outunit
   integer(kind=c_long) ::  start_time, end_time, count_rate
   real(kind=CF) :: r, res, tol, diff, xdiff
   real(kind=CF), dimension(:), allocatable :: b, x, x_ref
   real(kind=CF), dimension(:, :), allocatable :: a
   character(len=100) :: outfile
 
-  nmax = 0_CI
+  failures = 0_CI
+  maxdim = 30_CI
   call random_init(.true., .true.)
   call system_clock(start_time, count_rate)
   do i = 1, 1000
     call random_number(r)
-    ! at least 2 x 2 matrices
-    n = 2_CI + floor(20 * r) 
+    ! ensure matrix size is between 2 x 2 and maxdim x maxdim
+    n = 2_CI + floor((maxdim - 2_CI) * r) 
     call random_number(r)
-    m = 2_CI + floor(20 * r) 
+    m = 2_CI + floor((maxdim - 2_CI) * r) 
     mode = 0_CI
     res = 0.0_CF
     maxiter = 3 * n
@@ -33,23 +34,23 @@ program main
     call solve(a, b, x, m, n, mode, res, maxiter, tol)
     diff = norm2(matmul(A, x) - b)
     xdiff = norm2(x_ref - x)
-    if ((mode.eq.-1_CI).or.(diff.gt.1.0_CF)) then
+    if ((mode.eq.-1_CI).or.(diff.gt.tol)) then
       write(outfile, '(a, i0.4, a)') "out/info_", i, ".txt"
-      open(unit=20, file=outfile)
-      nmax = nmax + 1
-      write(20, *) "iteration ", i
-      write(20, *) "x_ref = ", x_ref
-      write(20, *) "x = ", x
-      write(20, *) "shape(A) = ", shape(A)
-      write(20, *) "A = "
+      open(newunit=outunit, file=outfile)
+      failures = failures + 1
+      write(outunit, *) "iteration ", i
+      write(outunit, *) "x_ref = ", x_ref
+      write(outunit, *) "x = ", x
+      write(outunit, *) "shape(A) = ", shape(A)
+      write(outunit, *) "A = "
       do j = 1, m
-        write(20, *) (A(j, k), k = 1, n)
+        write(outunit, *) (A(j, k), k = 1, n)
       end do
-      write(20, *)
-      write(20, *) "Ax = ", matmul(A, x)
-      write(20, *) "b = ", b
-      write(20, *) "diff = ", diff
-      close(20)
+      write(outunit, *)
+      write(outunit, *) "Ax = ", matmul(A, x)
+      write(outunit, *) "b = ", b
+      write(outunit, *) "diff = ", diff
+      close(outunit)
     else
       write(*, '(a, i4, a, G10.3, a, G10.3)') "i = ", i,&
         ": |Ax - b| = ", diff, ", |x_ref - x| = ", xdiff
@@ -60,8 +61,8 @@ program main
     deallocate(a)
   end do
   call system_clock(end_time)
-  write(*, *) "nmax = ", nmax
-  write(*, '(a, f10.6)') "time elapsed = ",&
+  write(*, *) "number of failures: ", failures
+  write(*, '(a, f10.6)') "time elapsed (s) = ",&
     real(end_time - start_time) / real(count_rate)
 
 end program main
